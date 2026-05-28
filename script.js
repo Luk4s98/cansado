c1aeb228dcfc76517fc737ff599c093bea9e92ae
Luk4s98 <lukasbarreto47@gmail.com>
2026-05-28 18:10:36 -0400

    aniversario-da-nenem

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function mostrarMensagem(){
+
+    const surpresa = document.getElementById("surpresa");
+
+    surpresa.classList.remove("hidden");
+
+    const musica = document.getElementById("musica")
+
+    musica.play();
+
+}
+
